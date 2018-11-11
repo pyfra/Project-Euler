f54919a88c3c6d76675ce87ee1e768bffdc8f445
pyfra <f.defano1@gmail.com>
2018-11-11 17:14:21 +0100

    improved algo

--- Python/problem_3.py
+++ Python/problem_3.py
@@ -1,31 +1,20 @@
-from util_euler import is_prime
-
-"""
-Slow solution
-
+from math import floor, sqrt
 TARGET_NUMBER = 600851475143
-primes = filter(lambda x: is_prime(x) and TARGET_NUMBER % x == 0, range(3, TARGET_NUMBER, 2))
-result = max(primes)
-print(result)
 
-"""
-
-#Faster solution, dividing every time by the first prime we find, drastically reduce the search space
-TARGET_NUMBER = 600851475143
 def get_max_prime(num):
-    #divisibility by 2 is not needed in this case as the number is odd, so the next step could be skipped. It is included for completeness
-    if num % 2 == 0:
-        if num // 2 == 1:
-            return 1
-        else:
-            num //= 2
+    #check whether it is divisible by 2, not needed for this problem as the number is clearly odd, but good for a general
+    #case
+    while num % 2 == 0:
+        num //= 2
+    if num == 1:
+        return 1
 
-    for x in range(3, num + 1, 2):
-        if is_prime(x) and num % x == 0:
+    for x in range(3, floor(sqrt(num)) + 1, 2):
+        while num % x == 0:
             num //= x
-            if num == 1:
-                return x
-            else:
-                return get_max_prime(num)
+        if num == 1:
+           return x
+    else:
+        return num
 
 result = get_max_prime(TARGET_NUMBER)
