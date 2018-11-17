254cf84d3ac0a2895d822ce687bc010d2ad2b18e
pyfra <f.defano1@gmail.com>
2018-11-17 19:23:43 +0100

    added solution problem 6

---
+++ Python/problem_6.py
@@ -0,0 +1,12 @@
+def sum_of_squares(n):
+    return n*(n+1)*(2*n+1)//6
+
+def squared_sum(n):
+    return (n * (n + 1) // 2)**2
+
+def diff_sums(n):
+    return squared_sum(n) - sum_of_squares(n)
+
+if __name__ == "__main__":
+    n = 100
+    print(diff_sums(n))
