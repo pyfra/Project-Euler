2bc127ab933ab13c4e17756ffb70e45064639021
FrankDF <f.defano1@gmail.com>
2019-12-05 18:10:34 +0100

    add solution problem_2 JavaScript

---
+++ JavaScript/problem_2.js
@@ -0,0 +1,28 @@
+function fibonacci(num, memo) {
+  memo = memo || {};
+
+  if (memo[num]) return memo[num];
+  if (num <= 1) return 1;
+  
+  memo[num] = fibonacci(num - 1, memo) + fibonacci(num - 2, memo);
+  return memo[num]
+}
+
+function euler2(limit){  
+    fibonacciNumbers = {}
+    let num, sum;
+    num = 2;
+    sum = 0;
+    currentFib = fibonacci(num, fibonacciNumbers);
+    while (currentFib <= limit){
+        if (currentFib % 2 === 0){
+            sum += currentFib;
+        }
+        num += 1;
+        currentFib = fibonacci(num, fibonacciNumbers);
+        //console.log(currentFib)
+    }
+    return sum
+
+}    
+console.log(euler2(4000000))
