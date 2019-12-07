5d8de6f2b7b0995977a8e330763e591e74be3f9b
FrankDF <f.defano1@gmail.com>
2019-12-07 15:33:23 +0100

    add solution problem 3

---
+++ JavaScript/problem_3.js
@@ -0,0 +1,16 @@
+function isPrime (num) {
+    for(let i = 2, s = Math.sqrt(num); i <= s; i++)
+        if(num % i === 0) return false; 
+    return num > 1;
+}
+
+function maxPrimeDivisor(num){
+    for (var i = num; i >= 0; i--) {
+	if ((num % i === 0) && (isPrime(i))){
+        return i;
+    }
+}
+}
+
+//console.log(maxPrimeDivisor(600851475143));
+console.log(maxPrimeDivisor(600851475143));
